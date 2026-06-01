cb489dc594310204b601540a471a8faff2535793
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:14:07 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Binary-Search-Tree-DSA.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:14:07Z'
+});
